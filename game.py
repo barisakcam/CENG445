822a845165b1c69b19a60e3432f8bfceaeee0e4b
KAAN GOCMEN <e2414894@ceng.metu.edu.tr>
2023-04-15 11:38:55 +0300

    a

--- game.py
+++ game.py
@@ -1,12 +1,28 @@
 from board import Board
-from cell import Cell
+from cell import Cell, Property
 from user import User
+import random
 
-def roll():
-    pass
+##şimdilik burada kalsın
 
-def buy():
-    pass
+def roll(board: Board ,user: User):
+    move = random.randint(1,6)
+    print(user.username, "tossed", move)
+    user.status.location_index += move
+    user.status.location_index %= len(board.cells)
+    print("kaan landed on", board.cells[user.status.location_index].type)
+    
+    return #bilmiyorum ne döner
+
+def buy(user: User, property: Property):
+    if user.status.money >= property.price:
+
+        user.status.money -= property.price
+        property.owner = User
+        user.status.properties.append(property)
+
+        return True
+    return False
 
 def upgrade():
     pass
@@ -21,6 +37,11 @@ def teleport(newcell):
     pass
 
 
-if __name__ == "__main__":
-    board = Board("input.json")
-    print(board)
+b = Board("input.json")
+u = User("kaan","k@k","sadfas","12345678")
+u.ready()
+u.status.start_game(b)
+roll(b,u)
+roll(b,u)
+roll(b,u)
+
